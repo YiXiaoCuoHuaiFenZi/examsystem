94b34d7a186c11f296fd11deddd40a40b2fb67be
gy_hu <gy_hu@Ctrip.com>
2015-12-29 18:36:51 +0800

    add admin sign up page

--- app/controllers/admin.go
+++ app/controllers/admin.go
@@ -14,6 +14,58 @@ func (this Admin) Index() revel.Result {
 	return this.Render()
 }
 
+func (this Admin) SignUp() revel.Result {
+	if this.Session["SignUpStatus"] == "true" {
+		this.RenderArgs["SignUpStatus"] = true
+		this.Session["SignUpStatus"] = "false"
+	}
+	return this.Render()
+}
+
+func (this Admin) PostSignUp(signUpAdmin *models.SignUpAdmin) revel.Result {
+//	this.Validation.Required(signUpAdmin.Name).Message("请输入考生姓名")
+//	this.Validation.Required(signUpAdmin.IDCard).Message("请输入身份证号码")
+//	this.Validation.Length(signUpAdmin.IDCard, 18).Message("身份证号有误")
+//	this.Validation.Required(signUpAdmin.Password).Message("请输入密码")
+//	this.Validation.Required(signUpAdmin.ConfirmPassword).Message("确认密码不能为空")
+//	this.Validation.MinSize(signUpAdmin.Password, 6).Message("密码长度不短于6位")
+//	this.Validation.Required(signUpAdmin.ConfirmPassword == signUpAdmin.Password).Message("两次输入的密码不一致")
+
+//	if this.Validation.HasErrors() {
+//		this.Validation.Keep()
+//		this.FlashParams()
+//		return this.Redirect((*Examinee).SignUp)
+//	}
+
+//	manager, err := models.NewDBManager()
+//	if err != nil {
+//		this.Response.Status = 500
+//		return this.RenderError(err)
+//	}
+//	defer manager.Close()
+
+//	err = manager.SignUp(signUpUser)
+//	if err != nil {
+//		this.Validation.Clear()
+
+//		// 添加错误信息，显示在页面的身份证下面
+//		var e revel.ValidationError
+//		e.Message = err.Error()
+//		e.Key = "signUpUser.IDCard"
+//		this.Validation.Errors = append(this.Validation.Errors, &e)
+
+//		this.Validation.Keep()
+//		this.FlashParams()
+//		return this.Redirect((*Examinee).SignUp)
+//	}
+
+//	this.Session["SignUpStatus"] = "true"
+//	log.Println("注册成功！")
+//	log.Println(signUpUser)
+
+	return this.Redirect((*Examinee).SignUp)
+}
+
 func (this Admin) SignIn() revel.Result {
 	return this.Render()
 }
--- app/models/structs.go
+++ app/models/structs.go
@@ -44,8 +44,9 @@ type SignInAdmin struct {
 
 // 管理员信息数据结构--真实数据
 type Admin struct {
-	IDCard string // 身份证号
-	Name   string // 管理员姓名
-	Gender string // 管理员性别
-	Level  int    // 管理员权限级别
+	IDCard   string // 身份证号
+	Name     string // 管理员姓名
+	Gender   string // 管理员性别
+	Password []byte // 账号密码
+	Level    int    // 管理员权限级别
 }
