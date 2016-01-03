f2da9791581b4b410a0d4024bfab4cebc4c633bd
xausee <xausee@gmail.com>
2016-01-03 22:42:14 +0800

    finish score query function for administrator

--- app/controllers/examPaper.go
+++ app/controllers/examPaper.go
@@ -1,8 +1,8 @@
 package controllers
 
 import (
-//	"ExamSystem/app/models"
-//	"log"
+	"ExamSystem/app/models"
+	"log"
 
 	"github.com/revel/revel"
 )
@@ -14,27 +14,65 @@ type ExamPaper struct {
 func (this ExamPaper) Create() revel.Result {
 	this.RenderArgs["adminIDCard"] = this.Session["adminIDCard"]
 	this.RenderArgs["adminName"] = this.Session["adminName"]
-	
+
 	return this.Render()
 }
 
 func (this ExamPaper) View() revel.Result {
 	this.RenderArgs["adminIDCard"] = this.Session["adminIDCard"]
 	this.RenderArgs["adminName"] = this.Session["adminName"]
-	
+
 	return this.Render()
 }
 
 func (this ExamPaper) Publish() revel.Result {
 	this.RenderArgs["adminIDCard"] = this.Session["adminIDCard"]
 	this.RenderArgs["adminName"] = this.Session["adminName"]
-	
+
 	return this.Render()
 }
 
-func (this ExamPaper) Score() revel.Result {
+func (this ExamPaper) Score(idCard string) revel.Result {
+	manager, err := models.NewDBManager()
+	if err != nil {
+		this.Response.Status = 500
+		return this.RenderError(err)
+	}
+	defer manager.Close()
+
+	examinee, e := manager.GetExamineeByIDCard(idCard)
+
+	if e != nil {
+		log.Println(e)
+		this.Response.Status = 500
+		return this.RenderError(e)
+	}
+
+	log.Println("查到考生信息: ", examinee)
+
+	this.RenderArgs["examinee"] = examinee
 	this.RenderArgs["adminIDCard"] = this.Session["adminIDCard"]
 	this.RenderArgs["adminName"] = this.Session["adminName"]
-	
+
 	return this.Render()
+}
+
+func (this ExamPaper) QueryScore() revel.Result {
+	this.RenderArgs["adminIDCard"] = this.Session["adminIDCard"]
+	this.RenderArgs["adminName"] = this.Session["adminName"]
+
+	return this.Render()
+}
+
+func (this ExamPaper) PostQueryScore(examineeIDCard string) revel.Result {
+	this.Validation.Required(examineeIDCard).Message("请输入身份证号码")
+
+	if this.Validation.HasErrors() {
+		this.Validation.Keep()
+		this.FlashParams()
+		return this.Redirect(ExamPaper.QueryScore)
+	}
+
+	return this.Redirect("/ExamPaper/Score/%s", examineeIDCard)
+	//return this.Redirect(ExamPaper.Score("huguangyue"))
 }
